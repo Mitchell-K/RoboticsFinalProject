4f1ed97e0f99365c1a3bb69695217696a0760312
Mitchell-K <mitchell.kuiper13@gmail.com>
2018-05-03 14:49:56 -0700

    Updated

--- src/final/src/move.cpp
+++ src/final/src/move.cpp
@@ -5,25 +5,49 @@
 #include <angles/angles.h>
 #include <tf2/LinearMath/Quaternion.h>
 #include <nav_msgs/OccupancyGrid.h>
+#include <vector>
 
 
 //Globals
-bool newGoal;
-bool sentGoal;
+bool newGoal, sentGoal;
 bool waitingForMap;
 nav_msgs::OccupancyGrid map;
 float curX = 0, curY = 0;
 
-bool isFree(float xx, float yy){
-    int xGrid = map.info.width/2 + xx/map.info.resolution;
-    int yGrid = map.info.height/2 + yy/map.info.resolution;
+// Create struct to hold simple map coordinates
+struct Cord{
+    float x;
+    float y;
+    int index;
+};
+std::vector<Cord> prevCords;
+
+// Checks if coordinate is free in cost map
+bool isFree(Cord tempCord){
+    int xGrid = map.info.width/2 + tempCord.x/map.info.resolution;
+    int yGrid = map.info.height/2 + tempCord.y/map.info.resolution;
     int i = xGrid + map.info.width*yGrid;
-    ROS_INFO_STREAM("Checking (" << xx << ", " << yy << ") on costmap");
+    //ROS_INFO_STREAM("Checking (" << tempCord.x << ", " << tempCord.y << ") on costmap" << i << '\t'<< (int)map.data[i]  );
     if ( (int)map.data[i] >= 90 | (int)map.data[i] == -1 ) {
 	ROS_INFO_STREAM("Bad coordinate");
 	return false;
     }
     return true;
+}
+// Checks if we've been near this coordinate before
+bool beenNearCord(const Cord tempCord){
+    if (sqrt( pow(0-tempCord.x,2) - pow(0-tempCord.y,2) ) < 3.0){
+	    ROS_INFO_STREAM("Too close to center");
+	    return true;
+        }
+    std::vector<Cord>::iterator it;
+    for(it = prevCords.begin(); it != prevCords.end(); it++)    {
+	if (sqrt( float(pow(it->x-tempCord.x,2)) - float(pow(it->y-tempCord.y,2)) ) < 2.0){
+	    ROS_INFO_STREAM("Been near, trying new cordinate.");
+	    return true;
+        }
+    }
+    return false;
 }
 
 void mapLoad(const nav_msgs::OccupancyGrid::ConstPtr& msg) {
@@ -41,6 +65,7 @@ void serviceActivated() {
 void serviceDone(const actionlib::SimpleClientGoalState& state, const move_base_msgs::MoveBaseResultConstPtr& result) {
     ROS_INFO_STREAM("Service completed");
     ROS_INFO_STREAM("Final state " << state.toString().c_str());
+    if (state.toString().c_str() == "ABORTED")prevCords.pop_back();
     newGoal = true; // enable receiving a new goal
     sentGoal = false; // enable to recieve new goal
     ROS_INFO_STREAM("Waiting for new taget pose...");
@@ -68,6 +93,11 @@ int main(int argc,char **argv) {
     }
     ROS_INFO_STREAM("done!");
 
+    if (waitingForMap){
+	ROS_INFO_STREAM("Waiting for map");
+    	ros::spinOnce();
+    }
+
     newGoal = true;
     sentGoal = false;
     bool firstStart = true;
@@ -75,12 +105,8 @@ int main(int argc,char **argv) {
 
     ros::Time startGoal;
 
-    float goalX, goalY;
-
-    while (ros::ok() ) {
-        if (waitingForMap){
-	    ROS_INFO_STREAM("Waiting for map");
-	}
+    Cord goalCord;
+    while (ros::ok() ) {        
         if (firstStart & !sentGoal){
 	    // Create goal for robot
     	    move_base_msgs::MoveBaseGoal goal;
@@ -90,47 +116,50 @@ int main(int argc,char **argv) {
 	    //Set goal coordinates
 	    switch(count) {
 		case 0: 
-    	    		goal.target_pose.pose.position.x = 8;
-    	    		goal.target_pose.pose.position.y = 8;
+			goalCord.x = 8.0;
+			goalCord.y = 8.0;
 			break;
 		case 1: 
-    	    		goal.target_pose.pose.position.x = 8;
-    	    		goal.target_pose.pose.position.y = -8;
+			goalCord.x = 8.0;
+			goalCord.y = -8.0;
 			break;
 		case 2: 
-    	    		goal.target_pose.pose.position.x = -8;
-    	    		goal.target_pose.pose.position.y = -8;
+			goalCord.x = -8.0;
+			goalCord.y = -8;
 			break;
 		case 3: 
-    	    		goal.target_pose.pose.position.x = -8;
-    	    		goal.target_pose.pose.position.y = 8;
+			goalCord.x = -8.0;
+			goalCord.y = 8.0;
 			//End of start
 			firstStart = false;
 			break;
 		default:
 			break;	    		
-	    }	    
+	    }
+	    goal.target_pose.pose.position.x = goalCord.x;
+    	    goal.target_pose.pose.position.y = goalCord.y;	    
 	    goal.target_pose.pose.orientation.w = 1;
-
 
 	    //Send goal to robot
 	    ROS_INFO_STREAM("Sending goal: Case: " << count++ );
+            prevCords.push_back(goalCord);
     	    ac.sendGoal(goal,&serviceDone,&serviceActivated,&serviceFeedback);
 	    startGoal = ros::Time::now();
 	    sentGoal = true;   
 	}
         // If we need a new pose wait
-	// *****This will need more loagic **************
 	else if (newGoal & !firstStart) {
 	    //int randNum = rand()%(max-min + 1) + min;
-	    float tempX = rand()%(17) - 8;
-	    float tempY = rand()%(17) - 8;
-	    ROS_INFO_STREAM("Attempting goal: (" << tempX << ", " << tempY << ")"  );
-	    bool coordSafe = isFree(tempX, tempY);
-            bool tooClose = sqrt( pow(tempX-curX,2) - pow(tempY-curY,2) ) < 3.0;
-	    if ( coordSafe && !tooClose ){
-		goalX = tempX;
-		goalY = tempY;
+	    Cord tempCord;
+	    tempCord.x = float(rand())/float(RAND_MAX) * 17 - 8;
+	    tempCord.y = float(rand())/float(RAND_MAX) * 17 - 8;
+	    ROS_INFO_STREAM("Attempting goal: (" << tempCord.x << ", " << tempCord.y << ")"  );
+	    bool coordSafe = isFree(tempCord);
+            bool tooClose = sqrt( pow(tempCord.x-curX,2) - pow(tempCord.y-curY,2) ) < 3.0;
+	    bool alreadyTried = beenNearCord(tempCord);
+	    if ( coordSafe && !tooClose && !alreadyTried){
+		goalCord.x = tempCord.x;
+		goalCord.y = tempCord.y;
 		newGoal = false;
 	    }
 	}
@@ -143,18 +172,19 @@ int main(int argc,char **argv) {
     	    goal.target_pose.header.stamp = ros::Time::now();
 			
 	    //Set goal coordinates
-    	    goal.target_pose.pose.position.x = goalX;
-    	    goal.target_pose.pose.position.y = goalY;
+    	    goal.target_pose.pose.position.x = goalCord.x;
+    	    goal.target_pose.pose.position.y = goalCord.y;
 	    goal.target_pose.pose.orientation.w = 1;
 
 
 	    //Send goal to robot
-	    ROS_INFO_STREAM("Sending goal: (" << goalX << ", " << goalY << ")"  );
+	    ROS_INFO_STREAM("Sending goal: (" << goalCord.x << ", " << goalCord.y << ")"  );
+	    prevCords.push_back(goalCord);
     	    ac.sendGoal(goal,&serviceDone,&serviceActivated,&serviceFeedback);
 	    startGoal = ros::Time::now();
 	    sentGoal = true;
 	}
-	else if (ros::Time::now() >= (startGoal + ros::Duration(45.0)) ){
+        else if (ros::Time::now() >= (startGoal + ros::Duration(45.0)) ){
 	    ROS_INFO_STREAM("Service running longer than 45 seconds, cancelling goal");
 	    ac.cancelGoal();
 	    // Set flags now to avoid delay during cancel call.
